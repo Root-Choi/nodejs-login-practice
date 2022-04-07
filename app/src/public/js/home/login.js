a6852beb1ace3ec9206cca6019eae0290b163e57
root <cgc940405@gmail.com>
2022-04-07 23:31:56 +0900

    handling HTML object with using DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,16 @@
 'use strict';
 
-console.log('hello');
-console.log('bye');
+const id = document.getElementById('id'),
+    psword = document.getElementById('psword'),
+    loginBtn = document.getElementById('btnLogin');
+
+loginBtn.addEventListener('click', login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+
+    console.log(req);
+}
